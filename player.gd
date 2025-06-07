f2ca86b9bc44ad4657ac5ad3e0f5669d842350c0
joacs <gaelmzl49@gmail.com>
2025-06-07 17:48:44 -0500

    Salto de personaje y eliminacion de algunos controles de movmiento

--- player.gd
+++ player.gd
@@ -1,40 +1,46 @@
 extends CharacterBody2D
 
 var tamano_pantalla
-@export var velocidad_jugador = 400
+@export var velocidad_jugador = 200
+@export var fuerza_salto = -400
+@export var gravedad = 900
 
 func _ready() -> void:
 	tamano_pantalla = get_viewport_rect().size
-	show()  # Asegura que esté visible
+	show()
 
-func _process(_delta: float) -> void:
-	var direccion = Vector2.ZERO
-	
+func _physics_process(delta: float) -> void:
+	var direccion_x = 0
+
+	# Movimiento lateral
 	if Input.is_action_pressed("mover_derecha"):
-		direccion.x += 1
+		direccion_x += 1
 	if Input.is_action_pressed("mover_izquierda"):
-		direccion.x -= 1
-	if Input.is_action_pressed("mover_arriba"):
-		direccion.y -= 1
-	if Input.is_action_pressed("mover_abajo"):
-		direccion.y += 1
+		direccion_x -= 1
+	
+	velocity.x = direccion_x * velocidad_jugador
 
-	if direccion.length() > 0:
-		direccion = direccion.normalized()
-		velocity = direccion * velocidad_jugador
-		_play_animacion(direccion)
+	# Aplicar gravedad
+	if not is_on_floor():
+		velocity.y += gravedad * delta
 	else:
-		velocity = Vector2.ZERO
+		# Salto
+		if Input.is_action_just_pressed("saltar"):
+			velocity.y = fuerza_salto
+
+	# Reproducir animación
+	if direccion_x != 0:
+		_play_animacion(Vector2(direccion_x, 0))
+	elif not is_on_floor():
+		$AnimatedSprite2D.play("Vox-down")  # Puedes cambiar esto a una animación de salto si tienes
+	else:
 		$AnimatedSprite2D.stop()
 
+	# Aplicar movimiento con física
 	move_and_slide()
 
 func _play_animacion(direccion: Vector2) -> void:
-	if direccion.y > 0:
-		$AnimatedSprite2D.play("Vox-down")
-	elif direccion.y < 0:
-		$AnimatedSprite2D.play("Vox-up")
-	elif direccion.x > 0:
+	if direccion.x > 0:
 		$AnimatedSprite2D.play("Vox-left")
 	elif direccion.x < 0:
 		$AnimatedSprite2D.play("Vox-right")
